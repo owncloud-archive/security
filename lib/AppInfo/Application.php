30e04845d6566ca982c3183ddcf710766b09fc91
Semih Serhat Karakaya <karakayasemi@itu.edu.tr>
2017-06-24 13:45:31 -0700

    Initialize structure, add sleep delay for BruteForce

---
+++ lib/AppInfo/Application.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ *
+ * @author Semih Serhat Karakaya
+ * @copyright Copyright (c) 2017, ownCloud GmbH.
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+namespace OCA\Security\AppInfo;
+
+use OC\AppFramework\Utility\TimeFactory;
+use \OCP\AppFramework\App;
+
+use \OCA\Security\Throttle;
+use \OCA\Security\Hooks;
+use \OCA\Security\Db\DBService;
+
+class Application extends App {
+
+    public function __construct(array $urlParams=array()){
+        parent::__construct('security', $urlParams);
+
+        $container = $this->getContainer();
+
+        $container->registerService('DBService', function($c) {
+            return new DBService(
+                $c->query('ServerContainer')->getDb(),
+				new TimeFactory()
+            );
+        });
+
+        $container->registerService('Throttle', function($c) {
+            return new Throttle(
+                $c->query('DBService')
+            );
+        });
+
+
+        $container->registerService('Hooks', function($c) {
+            return new Hooks(
+                $c->query('ServerContainer')->getUserManager(),
+                $c->query('Throttle')
+            );
+        });
+
+    }
+}
---
+++ lib/Hooks.php
@@ -0,0 +1,79 @@
+<?php
+/**
+
+ *
+ * @author Semih Serhat Karakaya
+ * @copyright Copyright (c) 2016, ITU IT HEAD OFFICE.
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+namespace OCA\Security;
+
+use OCP\IUser;
+use OCP\IUserManager;
+
+/**
+ * Class Hooks
+ * @package OCA\Secuity\Lib
+ */
+class Hooks {
+
+    /** @var \OC\User\Manager */
+    private $userManager;
+
+    /** @var Throttle*/
+    private $throttle;
+
+    /**
+     * @param IUserManager $userManager
+     * @param Throttle $throttle
+     */
+    public function __construct($userManager, $throttle){
+        $this->userManager = $userManager;
+        $this->throttle = $throttle;
+    }
+
+    public function register() {
+        $this->userManager->listen('\OC\User', 'preLogin', function($uid, $password) {
+            $this->preLoginCallback($uid, $password);
+        });
+
+        $this->userManager->listen('\OC\User', 'postLogin', function($user) {
+            $this->postLoginCallback($user);
+        });
+    }
+
+    /**
+     * @param string $uid
+     * @param Throttle $throttle
+     */
+    private function preLoginCallback($uid, $password) {
+        $ip = \OC::$server->getRequest()->getRemoteAddress();
+
+        if(\OC_User::checkPassword($uid, $password) === false) {
+            $this->throttle->addFailedLoginAttempt($uid, $ip);
+			$this->throttle->putDelay($uid, $ip);
+        }
+    }
+
+    /**
+     * @param IUser $user
+     */
+    private function postLoginCallback($user) {
+		$ip = \OC::$server->getRequest()->getRemoteAddress();
+    	$this->throttle->clearSuspiciousAttemptsForIp($ip);
+    }
+}
