5cb1e7f72d2e5040abb8b746dbd4f81cfebf7fa8
ownCloud Jenkins <jenkins@owncloud.com>
2017-08-17 01:07:11 +0000

    [tx-robot] updated from transifex

---
+++ l10n/pl.js
@@ -0,0 +1,22 @@
+OC.L10N.register(
+    "security",
+    {
+    "Password needs to be at least %s characters long" : "Hasło musi mieć co najmniej %s znaków",
+    "Password should contain at least one upper and one lower case character." : "Hasło powinno zawierać co najmniej jeden wielki i jeden mały znak.",
+    "Password should contain at least one numerical character." : "Hasło powinno zawierać co najmniej jeden znak numeryczny.",
+    "Password should contain at least one special character." : "Hasło powinno zawierać co najmniej jeden znak specjalny.",
+    "Brute Force Protection" : "Ochrona przed Brute Force",
+    "Activate Brute Force Protection" : "Aktywuj ochronę przed Brute Force",
+    "Password Policy" : "Polityka haseł",
+    "Determine minimum password length" : "Określ minimalną długość hasła",
+    "Save Length" : "Bezpieczna długość",
+    "Enforce at least one upper and one lower case character on passwords" : "Wymuszaj co najmniej jeden wielki i jeden mały znak w hasłach",
+    "Enforce at least one numerical characters on passwords" : "Wymuszaj co najmniej jeden znak numeryczny w hasłach",
+    "Enforce at least one special characters on passwords" : "Wymuszaj co najmniej jeden znak specjalny w hasłach",
+    "Passwords must:" : "Hasła muszą:",
+    "be at least 8 characters long " : "Co najmniej 8 znaków",
+    "contain at least one uppercase letter and at least one lowercase letter " : "zawierać co najmniej jedną wielką literę i co najmniej jedną małą literę",
+    "contain at least one numerical character" : "zawierać co najmniej jeden znak liczbowy",
+    "contain at least one special character" : "zawierać co najmniej jeden znak specjalny"
+},
+"nplurals=4; plural=(n==1 ? 0 : (n%10>=2 && n%10<=4) && (n%100<12 || n%100>14) ? 1 : n!=1 && (n%10>=0 && n%10<=1) || (n%10>=5 && n%10<=9) || (n%100>=12 && n%100<=14) ? 2 : 3);");
---
+++ l10n/ru.js
@@ -0,0 +1,22 @@
+OC.L10N.register(
+    "security",
+    {
+    "Password needs to be at least %s characters long" : "Пароль должен быть как минимум длиной в %s символов",
+    "Password should contain at least one upper and one lower case character." : "Пароль должен содержать как минимум одну заглавную и одну прописную букву.",
+    "Password should contain at least one numerical character." : "Пароль должен содержать как минимум одну цифру.",
+    "Password should contain at least one special character." : "Пароль должен содержать как минимум один специальный символ.",
+    "Brute Force Protection" : "Защита от прямого перебора",
+    "Activate Brute Force Protection" : "Активировать защиту от прямого перебора",
+    "Password Policy" : "Политика пароля",
+    "Determine minimum password length" : "Определить минимальную длину пароля",
+    "Save Length" : "Сохранить длину",
+    "Enforce at least one upper and one lower case character on passwords" : "Требовать как минимум одну заглавную и одну прописную букву в паролях",
+    "Enforce at least one numerical characters on passwords" : "Требовать как минимум одну цифру в паролях",
+    "Enforce at least one special characters on passwords" : "Требовать как минимум один специальный символ в паролях",
+    "Passwords must:" : "Пароли должны:",
+    "be at least 8 characters long " : "быть по крайней мере длиной в 8 символов",
+    "contain at least one uppercase letter and at least one lowercase letter " : "содержать как минимум одну заглавную и как минимум одну прописную букву",
+    "contain at least one numerical character" : "содержать как минимум одну цифру",
+    "contain at least one special character" : "содержать как минимум один специальный символ"
+},
+"nplurals=4; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<12 || n%100>14) ? 1 : n%10==0 || (n%10>=5 && n%10<=9) || (n%100>=11 && n%100<=14)? 2 : 3);");
