5d75e9b2f642ea7217b68f58950304fce2d2b8d6
ownCloud Jenkins <jenkins@owncloud.com>
2017-08-18 01:07:11 +0000

    [tx-robot] updated from transifex

---
+++ l10n/ar.js
@@ -0,0 +1,22 @@
+OC.L10N.register(
+    "security",
+    {
+    "Password needs to be at least %s characters long" : "يجب أن تتكون كلمة السر من %s أحرف",
+    "Password should contain at least one upper and one lower case character." : "يجب أن تحوي كلمة السر حرفاً كبيراً وحرفاً صغيراً على الأقل.",
+    "Password should contain at least one numerical character." : "يجب أن تحوي كلمة السر رقماً واحداً على الأقل.",
+    "Password should contain at least one special character." : "يجب أن تحوي كلمة السر حرفاً خاصاً على الأقل.",
+    "Brute Force Protection" : "حماية ضد هجمات القوة القسرية",
+    "Activate Brute Force Protection" : "تفعيل الحماية ضد القوة القسرية",
+    "Password Policy" : "سياسة كلمة السر",
+    "Determine minimum password length" : "تحديد أقصر طول لكلمة السر",
+    "Save Length" : "حفظ الطول",
+    "Enforce at least one upper and one lower case character on passwords" : "إلزام احتواء كلمة السر حرفاً كبيراً وحرفاً صغيراً على الأقل",
+    "Enforce at least one numerical characters on passwords" : "إلزام احتواء كلمة السر رقماً واحداً على الأقل",
+    "Enforce at least one special characters on passwords" : "إلزام احتواء كلمة السر حرفاً خاصاً على الأقل",
+    "Passwords must:" : "يجب في كلمة السر:",
+    "be at least 8 characters long " : "أن يكون طولها ٨ حروف على الأقل",
+    "contain at least one uppercase letter and at least one lowercase letter " : "أن تحوي حرفاً كبيراً وحرفاً صغيراً على الأقل.",
+    "contain at least one numerical character" : "أن تحوي رقماً على الأقل",
+    "contain at least one special character" : "أن تحوي حرفاً خاصاً على الأقل"
+},
+"nplurals=6; plural=n==0 ? 0 : n==1 ? 1 : n==2 ? 2 : n%100>=3 && n%100<=10 ? 3 : n%100>=11 && n%100<=99 ? 4 : 5;");
