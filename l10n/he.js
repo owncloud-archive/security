d76b33794ecb21b6b209e9dca5aff5ce6ba4f022
ownCloud Jenkins <jenkins@owncloud.com>
2017-08-19 01:07:12 +0000

    [tx-robot] updated from transifex

---
+++ l10n/he.js
@@ -0,0 +1,22 @@
+OC.L10N.register(
+    "security",
+    {
+    "Password needs to be at least %s characters long" : "סיסמא חייבת להכיל לפחות %s תווים",
+    "Password should contain at least one upper and one lower case character." : "סיסמא צריכה להכיל לפחות אות קטנה אחת ואות גדולה אחת.",
+    "Password should contain at least one numerical character." : "סיסמא צריכה לכלות לפחות סיפרה אחת.",
+    "Password should contain at least one special character." : "סיסמא צריכה לכלול לפחות תו מיוחד אחד.",
+    "Brute Force Protection" : "הגנת כוח גס - Brute Force",
+    "Activate Brute Force Protection" : "הפעלת הגנת כח גס",
+    "Password Policy" : "מדיניות סיסמאות",
+    "Determine minimum password length" : "קביעת אורך סיסמא מינימלי",
+    "Save Length" : "שמירת אורך",
+    "Enforce at least one upper and one lower case character on passwords" : "אכיפת כלל של לפחות אות קטנה אחת ואות גדולה אחת בסיסמאות",
+    "Enforce at least one numerical characters on passwords" : "אכיפת כלל של לפחות סיפרה אחת בסיסמאות",
+    "Enforce at least one special characters on passwords" : "אכיפת כלל של לפחות תו מיוחד אחד בסיסמאות",
+    "Passwords must:" : "סיסמאות חייבות להכיל:",
+    "be at least 8 characters long " : "8 תווים לפחות",
+    "contain at least one uppercase letter and at least one lowercase letter " : "לפחות אות קטנה אחת ואות גדולה אחת",
+    "contain at least one numerical character" : "לפחות סיפרה אחת",
+    "contain at least one special character" : "לפחות תו מיוחד אחד"
+},
+"nplurals=2; plural=(n != 1);");
