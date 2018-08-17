987184f8a322763bf4cc8b089d683973b7b1b75b
ownCloud Jenkins <jenkins@owncloud.com>
2018-08-17 01:07:18 +0000

    [tx-robot] updated from transifex

---
+++ l10n/fi_FI.js
@@ -0,0 +1,28 @@
+OC.L10N.register(
+    "security",
+    {
+    "Preferences are saved" : "Asetukset on tallennettu",
+    "Length is saved" : "Pituus on tallennettu",
+    "Password needs to be at least %s characters long" : "Salasanan tulee olla vähintään %s merkkiä pitkä",
+    "Password should contain at least one upper and one lower case character." : "Salasanan tukee sisältää vähintään yksi iso kirjain ja yksi pieni kirjain.",
+    "Password should contain at least one numerical character." : "Salasanan tulee sisältää vähintää yksi numero",
+    "Password should contain at least one special character." : "Salasanan tulee sisältää vähintään yksi erikoismerkki",
+    "Too many failed login attempts. Try again in %s minutes." : "Liian monta kirjautumisyritystä. Yritä uudelleen %s minuutin jälkeen.",
+    "Brute Force Protection" : "Brute Force suojaus",
+    "Count failed login attempts over how many seconds?" : "Laske epäonnistuneet kirjautumiset seuraavan sekuntien sisällä?",
+    "Ban after how many failed login attempts?" : "Estä kun seuraava määrä epäonnistuneita kirjautumisia?",
+    "Ban for how many seconds?" : "Esto kestää sekunneissa?",
+    "Save settings" : "Tallenna asetukset",
+    "Password Policy" : "Salasana politiikka",
+    "Determine minimum password length" : "Määritä salasanan vähimmäispituus",
+    "Save length" : "Tallenna pituus",
+    "Enforce at least one upper and one lower case character on passwords" : "Pakota vähintään yksi iso kirjain ja yksi pieni kirjain salasanassa",
+    "Enforce at least one numerical characters on passwords" : "Pakota vähintään yksi numero salasanassa",
+    "Enforce at least one special characters on passwords" : "Pakota vähintään yksi erikoismerkki salasanassa",
+    "Passwords must:" : "Salasanan tulee:",
+    "be at least %s characters long " : "Olla vähintään %s merkkiä pitkä",
+    "contain at least one uppercase letter and at least one lowercase letter " : "Sisältää vähintään yksi iso kirjain ja yksi pieni kirjain",
+    "contain at least one numerical character" : "Sisältää vähintään yksi numero",
+    "contain at least one special character" : "Sisältää vähintään yksi erikoismerkki"
+},
+"nplurals=2; plural=(n != 1);");
